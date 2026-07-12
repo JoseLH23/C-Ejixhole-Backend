2b451d6006878ac19dbc61310cfe36ba0f031aa0
JoseLH23 <chepo23larios@gmail.com>
2026-07-11 20:30:46 -0600

    Agregar dominio Vercel a CORS

--- app/main.py
+++ app/main.py
@@ -19,6 +19,7 @@ app.add_middleware(
         "http://localhost:5174",
         "http://127.0.0.1:5174",
         "https://reservas.ejixhole.com",
+        "https://ejixhole-reservas.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
